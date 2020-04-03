eb4319ccc30260cbcd15a0fbdd62240fced728f5
timrager <tim.rager@gmail.com>
2020-04-02 20:38:51 -0700

    project updates

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 
 
 $(() => {
-
-    // global vars
-
+    //////////////////////
+    // GLOBAL VARIABLES //
+    //////////////////////
     let firstClick = null; // capture class of first on Click
     let clickCheck = null; // check to see if 2 clicks captured
     let secondClick = null; // capture class of second click
-    let playerOneScore = 0;
-    let playerTwoScore = 0;
-    let playerTurn = 1;
-    let roundDone = null;
-    let clickTarget1 = null;
-    let clickTarget2 = null;
-    let matchCounter = 10;
+    let playerOneScore = 0; // keep score for the 1st player
+    let playerTwoScore = 0; // keep score for the 2nd player
+    let playerTurn = 1; // keep track of who's turn it is
+    let roundDone = null; // flag to track if the current turn is done
+    let clickTarget1 = null;  // capture the target of a click in a variable
+    let clickTarget2 = null; // capture the target of a click in a variable
+    let matchCounter = 10; // count and track how many matches are available in the deck
 
+    // array of cards to use
     const cardArr = [
         {
             suit: "hearts",
@@ -47,34 +48,34 @@ $(() => {
             symbol: "♥"
         },
         {
-            suit: "hearts",
+            suit: "spades",
             name: "six",
             value: 6,
-            symbol: "♥"
+            symbol: "♠"
         },
         {
-            suit: "hearts",
+            suit: "spades",
             name: "seven",
             value: 7,
-            symbol: "♥"
+            symbol: "♠"
         },
         {
-            suit: "hearts",
+            suit: "spades",
             name: "eight",
             value: 8,
-            symbol: "♥"
+            symbol: "♠"
         },
         {
-            suit: "hearts",
+            suit: "spades",
             name: "nine",
             value: 9,
-            symbol: "♥"
+            symbol: "♠"
         },
         {
-            suit: "hearts",
+            suit: "spades",
             name: "ten",
             value: 10,
-            symbol: "♥"
+            symbol: "♠"
         }
     ];
 
@@ -83,7 +84,30 @@ $(() => {
    
     // cached dom elements
     const gameBoard = $('.gameboard');
+    // Grabbing About the Game button
+    const $openBtn = $('#openModal');
+    // Grabbing modal element
+    const $modal = $('#modal');
+    // Grabbing close button
+    const $closeBtn = $('#close');
 
+    
+    // Event handler to open the modal
+    const openModal = () => {
+        $modal.css('display', 'block');
+    }
+    // Event handler to close the modal
+    const closeModal = () => {
+        $modal.css('display', 'none');
+    }
+    //Add event listener to Close button
+    $closeBtn.on('click', closeModal);
+    //Add event listener to About the Game button
+    $openBtn.on('click', openModal);
+
+    ///////////////
+    // FUNCTIONS //
+    ///////////////
     const generateCards = () => {
 
         $('.currPlayer').append().text(`Current Player: ${playerTurn}`);
@@ -97,12 +121,12 @@ $(() => {
 
             // append the card to the gameboard div
             $(gameBoard).append($card);
-        }
-    }
+        } // end of card generation
+    } // end of generateCards()
 
+    // function to shuffle the deck array
     const shuffleDeck = () => {
         
-
         // Durstenfeld's version of the Fisher-Yates shuffle algorithm
         // https://en.wikipedia.org/wiki/Fisher–Yates_shuffle#The_modern_algorithm
         for (let i = newDeck.length-1; i >= 0; i--){
@@ -120,7 +144,7 @@ $(() => {
 
             // jQuery render card values - front of card
             let placeholder = `location${i}`;
-            $(`.${placeholder}`).append(`<div class="frontCard ${newDeck[i].name}">`);
+            $(`.${placeholder}`).append(`<div class="frontCard ${newDeck[i].name} ${newDeck[i].suit} ">`);
             $(`.${placeholder}`).children().append(`<p>${newDeck[i].value}</p>`);
             $(`.${placeholder}`).children().append(`<p>${newDeck[i].symbol}</p>`);
             $(`.${placeholder}`).children().addClass('hideCard');
@@ -130,44 +154,19 @@ $(() => {
         } // end for loop
     } // end shuffleDeck()
 
-    // const shuffleDeckSecond = () => {
-        
-    //     // Durstenfeld's version of the Fisher-Yates shuffle algorithm
-    //     // https://en.wikipedia.org/wiki/Fisher–Yates_shuffle#The_modern_algorithm
-    //     for (let i = cardArr.length-1; i >= 0; i--){
-
-    //         let scratchNum = 0;
-    //         let resultNum = 0;
-
-    //         let rollNum = (Math.floor(Math.random() * i));
-    //         // let rollNumSecondHalf = (Math.floor(Math.random() * i) + 10);
-
-    //         // swap out cardArr index with random place in array
-    //         resultNum = cardArr[rollNum];
-    //         scratchNum = cardArr[i];
-    //         cardArr[rollNum] = scratchNum;
-    //         cardArr[i] = resultNum;
-
-    //         // jQuery render card values
-    //         let placeholder = `location${i+10}`;
-    //         $(`.${placeholder}`).append(`<div class="frontCard ${cardArr[i].name}">`);
-    //         $(`.${placeholder}`).children().append(`<p>${cardArr[i].value}</p>`);
-    //         $(`.${placeholder}`).children().append(`<p>${cardArr[i].symbol}</p>`);
-    //         $(`.${placeholder}`).append('<div class="backCard">');
-
-    //     } // end for loop
-        
-    // } // end shuffleDeckSecond()
-
+    // function to create onClick event to play the game
     const playMatch = () => {
 
+        // create onClick event on each .card
         $('.card').on('click', (event) => {
             
+            // stop page from reloading
             event.preventDefault();
 
+            // split out the classes and set to value var
             let $value = $(event.currentTarget).children().attr("class").split(' ');
             
-
+            // logic to check if you are in turn 1 or turn 2
             if (clickCheck === true){ 
                 // checkForSecondClick = $(event.currentTarget).addClass('clicked')
                 $(event.currentTarget).addClass('noMoreClicks')
@@ -183,21 +182,14 @@ $(() => {
                 firstClick = $value[1];            
                 clickCheck = true;
             }
-
-            // console.log(firstClick);
-            // console.log(secondClick);
-
-
             // make sure to only capture 2 clicks.  Otherwise 3rd click should be "first" click.
             if (firstClick !== null && secondClick !==null){
                 checkRound();
                 firstClick = null;
                 secondClick = null;
                 clickCheck = false;
-            }
-
+            } // end of if clickCheck
         }) // end of on click
-        
     } // end of playMatch
 
     const checkRound = () => {
@@ -225,8 +217,6 @@ $(() => {
                 if (matchCounter === 0) {
                     endMatch();
                 }
-
-
                 console.log(matchCounter);
             } else {
                 // $('.clicked').removeClass('clicked')
@@ -248,65 +238,41 @@ $(() => {
                 
             }
         $('.card').removeClass('noMoreClicks')
-
-        }
-
-    }
+        } // end if for roundDone
+    } // end checkRound()
 
     const endMatch = () => {
+        // check to see who has the highest score
+        if (playerOneScore > playerTwoScore){
+            alert ("Player One Wins!");
+        } else if (playerOneScore < playerTwoScore){
+            alert ("Player Two Wins!");
+        } else {
+            alert ("It's a TIE!");
+        }
+        // prompt to start game again or quit
         let playAgain = prompt("Want to play again?", "Y/N")
         if (playAgain === "Y" || playAgain === "y") {
             location.reload(true);
         } else {
             return;
-        }
-    }
+        } // end if statement
+    } // end endMatch()
 
 
 // end of functions
 
-    // jQuery draw cards
-    generateCards();
+//////////////////////////////////
+// FUNCTION CALLS TO START GAME //
+//////////////////////////////////
 
-    // randomize deck (2x)
-    shuffleDeck();
-    //shuffleDeckSecond();
+// jQuery draw cards
+generateCards();
 
-    playMatch();
+// randomize deck (2x)
+shuffleDeck();
 
-
-    // $('.beginGame').on('click', () => {
-    //     console.log('go');
-    //     playMatch();
-    // });
-    
-
-
-
-
-
+// play the game
+playMatch();
 
 }) // end of jQuery onLoad
-
-
-
-
-
-// Game must have:
-
-// Must be a two player game (either against the computer or against another player)
-// Example: Blackjack: A player plays against the dealer. The dealer is the computer
-// Example: Connect Four: Two players pass the game between themselves to take turns
-
-// A win state - a way for the player to win the game
-// High score can be considered a win state 
-
-// A lose state - a way for the player to lose the game
-// Example: Blackjack - a player must be able to lose all of their money with losing hands and cannot play if their bankroll is at 0
-// Example: Connect Four - the other player has won or there are no possible plays left
-
-// A way to keep playing if the game is not over
-
-// Multiple rounds to play - a round must begin, end, and there must be a way to check if the game should continue or the overall game is won or lost
-// Example: Blackjack: a player takes turns playing a hand versus a computer - the player's hand can either win, lose or tie the dealer. If the player has enough money in their bankroll they can keep playing. A player must be able to win several rounds and increase their bankroll
-// Example: Connect Four: two (non-computer) players take turns adding chips to the board. The game will check if a player won or if the board is full and there are no more plays possible. A player gets four chips in a row (vertically or horizontally)- one person wins, one loses, there are no further plays in this case
